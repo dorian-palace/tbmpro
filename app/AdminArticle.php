c63a88b3d8b842c5b738498eff57b8ca32bf51a7
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-05-30 18:46:48 +0200

    adminArticle class

---
+++ app/AdminArticle.php
@@ -0,0 +1,29 @@
+<?php
+
+class AdminArticle extends Database{
+
+function __construct(){
+    parent::__construct();
+}
+
+function getArticleById(int $id){
+
+    $sql = "SELECT * FROM articles INNER JOIN images WHERE articles.id_images = images.id";
+
+    $request = $this->pdo->prepare($sql);
+    $request->execute();
+    $article = $request->fetch();
+
+    return $article;
+
+}
+
+function createArticle(){
+
+    $sql = "INSERT INTO articles (`text`)";
+}
+
+}
+
+$artnew = new AdminArticle();
+$artnew->getArticleById(7);
