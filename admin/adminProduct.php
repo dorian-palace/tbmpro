5fdcc899bcd6cf3e91dc5f20ae8325a7e8ef7034
[laura-scognamiglio] <91824380+laura-scognamiglio@users.noreply.github.com>
2022-07-21 22:48:13 +0200

    css admin module robot aside et main

--- admin/adminProduct.php
+++ admin/adminProduct.php
@@ -44,29 +44,40 @@ $total = ceil($nbStart / $limit);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../layouts/a.css">
+    <script type="text/javascript" src="../layouts/scriptNav.js"></script>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="icon" type="image/x-icon" href="../assets/img/favIcon.ico">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="../js/adminRobots.js"></script>
-    <link rel="stylesheet" href="css.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
     <title>Admin-product</title>
 </head>
 
-<body>
-    <ul class="link-to">
-        <li><a href="">Manage Robot</a></li>
-        <li><a href="">Add head & body to robot</a></li>
-        <li><a href="">Manage Categories</a></li>
-        <li><a href="#form-new-materials">Manage materials</a></li>
-        <li><a href="#form-new-color">Manage colors</a></li>
-    </ul>
-
+<body class = body-robot>
+    <header>
+        <nav class="topnav robot-nav" id="myTopnav">
+            <ul class="link-to myLinks">
+                <a href="" class="active">Accueil admin</a>
+                <a href="" class="link">Robot</a>
+                <a href="#form-new-cat" class="link">Categories</a>
+                <a href="#form-new-materials" class="link">Matières</a>
+                <a href="#form-new-color" class="link">Couleurs</a>
+               <a href="../setting/deconnexion.php" class="link">Deconnexion</a>
+               <a 
+                class="icon" >
+                <i class="fa fa-bars"></i>
+                </a>
+            </ul>
+        </nav>
+    </header>
+    <main class = "main-robot">
     <!---ROBOT-->
-    <div class="container-new-robot">
-        <label for="">New robot</label>
-
+    <aside>
+    <section class = "new-robot-section">
+        <article class= "first">
+        <label>Nouveau robot</label>
         <div class="container-filter-robot" id="container-filter-robot">
             <div class="container" id="container" for="label">
                 <!--append here-->
@@ -74,43 +85,40 @@ $total = ceil($nbStart / $limit);
         </div>
 
         <div class="mb-3">
-            <input type="text" name="name-robot" id="name-robot" placeholder="name-robot">
+            <input type="text" name="name-robot" id="name-robot" placeholder="nom du futur robot">
         </div>
 
+        <label>Couleur diponible</label>
         <div id="divParent">
             <?php
             foreach ($color as $allColor) :
             ?>
                 <button class="filter-color" id="select_color_<?= $allColor['id']; ?>"><?= $allColor['name']; ?></button>
-
-
             <?php endforeach; ?>
         </div>
-
+        <label>Matière disponible</label>
         <div class="mb-3">
-
-            <?php foreach ($material as $mat) : ?>
+            <?php foreach ($material as $mat): ?>
                 <button class="filter-mat" id="select_mat_<?= $mat['id']; ?>"><?= $mat['type']; ?></button>
             <?php endforeach; ?>
-
-
         </div>
 
         <div class="mb-3">
             <select name="categorieNewRobot" id="categorieNewRobot">
-                <?php foreach ($getCategories as $cat) : ?>
+                <?php foreach ($getCategories as $cat): ?>
                     <option value="<?= $cat['id']; ?>">
                         <?= $cat['name']; ?>
                     </option>
                 <?php endforeach; ?>
             </select>
         </div>
-        <button type="submit" value="submit-robot" class="btn btn-primary" name="submit-robot" id="submit-robot"> submit</button>
+        <button type="submit" value="submit-robot" class="btn btn-primary btn-action" name="submit-robot" id="submit-robot">submit</button>
     </div>
-
+    </article>
     <!---LAYER ROBOT--->
+    <article>
     <form action="" class="form-new-head" method="post" enctype="multipart/form-data">
-        <label for="">Add new Head</label>
+        <label for="">Ajout tête</label>
         <input type="file" name="image-head-robot">
 
         <select name="head-color" id="">
@@ -133,7 +141,7 @@ $total = ceil($nbStart / $limit);
     </form>
 
     <form action="" class="form-new-body" method="post" enctype="multipart/form-data">
-        <label for="">Add new body</label>
+        <label for="">Ajout corps</label>
         <input type="file" name="image-body-robot">
 
         <select name="body-color" id="">
@@ -154,58 +162,38 @@ $total = ceil($nbStart / $limit);
 
         <input type="submit" name="submit-body-robot" value="Add">
     </form>
+    </article>
+    </section>
 
     <!---DELETE LAYER ROBOT--->
-    <div class="container-delete-head">
-        <fieldset>
-            <label for="">Delete Head</label>
-            <?php foreach ($headRobot as $head) :  ?>
-                <div class="container-delete-head">
-                    <img src="../assets/<?= $head['head_name']; ?>" class="display-robot-head" alt="">
-                    <button name="delete-head" value="<?= $head['head_id']; ?>">Delete head</button>
-                </div>
-            <?php endforeach; ?>
-        </fieldset>
+    <div class="miniature">
+        <div class="container-delete-head">
+            <fieldset>
+                <label for="">Delete Head</label>
+                <?php foreach ($headRobot as $head) :  ?>
+                    <div class="container-delete-head">
+                        <img src="../assets/<?= $head['head_name']; ?>" class="display-robot-head" alt="">
+                        <button name="delete-head" value="<?= $head['head_id']; ?>">Delete head</button>
+                    </div>
+                <?php endforeach; ?>
+            </fieldset>
+        </div>
+
+        <div class="container-delete-body">
+            <fieldset>
+                <label for="">Delete Body</label>
+                <?php foreach ($bodyRobot as $body) : ?>
+
+                    <img src="../assets/<?= $body['body_name']; ?>" class="display-robot-head" alt="">
+                    <button name="delete-body" value="<?= $body['body_id']; ?>">Delete body</button>
+                <?php endforeach; ?>
+            </fieldset>
+        </div>
     </div>
-
-    <div class="container-delete-body">
-        <fieldset>
-            <label for="">Delete Body</label>
-            <?php foreach ($bodyRobot as $body) : ?>
-
-                <img src="../assets/<?= $body['body_name']; ?>" class="display-robot-head" alt="">
-                <button name="delete-body" value="<?= $body['body_id']; ?>">Delete body</button>
-            <?php endforeach; ?>
-        </fieldset>
-    </div>
-
+    
     <!----FIN DELETE ROBOT-->
 
 
-
-    <?php foreach ($allRobots as $nbRobots) : ?>
-
-        <div class="display-robot">
-            <div class="display-robot-name">
-                <b> Nom du robot <?= $nbRobots['name_robot']; ?> </b>
-            </div>
-            <div class="display-robot-material">
-
-                <b> crée par <?= $nbRobots['surname'] ?></b>
-
-            </div>
-            <!-- <div class="display-robot-head"> -->
-            <img src="../assets/<?= $nbRobots['head_name']; ?>" alt="" class="display-robot-head">
-            <!-- </div> -->
-            <!-- <div class="display-robot-body"> -->
-            <img src="../assets/<?= $nbRobots['body_name']; ?>" alt="" class="display-robot-body">
-            <!-- </div> -->
-            <div class="display-robot-delete">
-                <button type="submit" name="delete-robot" class="delete-robot" id="delete-robot" value="<?= $nbRobots['id_robot']; ?>">DELETE</button>
-            </div>
-        </div>
-    <?php
-    endforeach; ?>
 
     <ul class="pagination" style="align-items:center;">
         <li class="disabled"><?php if ($page > 1) { ?><a href="?page=<?= $page - 1  ?>"><i class="material-icons">
@@ -320,8 +308,33 @@ $total = ceil($nbStart / $limit);
             <button type="submit" id="button-delete" class="btn btn-danger" name="button_delete">Delete</button>
         </div>
         <!-- </form> -->
+        </aside>
 
 
+    <?php foreach ($allRobots as $nbRobots) : ?>
+
+<div class="display-robot">
+    <div class="display-robot-name">
+        <b> Nom du robot <?= $nbRobots['name_robot']; ?> </b>
+    </div>
+    <div class="display-robot-material">
+
+        <b> crée par <?= $nbRobots['surname'] ?></b>
+
+    </div>
+    <!-- <div class="display-robot-head"> -->
+    <img src="../assets/<?= $nbRobots['head_name']; ?>" alt="" class="display-robot-head">
+    <!-- </div> -->
+    <!-- <div class="display-robot-body"> -->
+    <img src="../assets/<?= $nbRobots['body_name']; ?>" alt="" class="display-robot-body">
+    <!-- </div> -->
+    <div class="display-robot-delete">
+        <button type="submit" name="delete-robot" class="delete-robot" id="delete-robot" value="<?= $nbRobots['id_robot']; ?>">DELETE</button>
+    </div>
+</div>
+<?php
+endforeach; ?>
+</main>
 </body>
 
 </html>
