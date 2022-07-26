c802e600fecd8c211b276eb0a4c7b0685c95a8f6
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-26 18:04:47 +0200

    stats combinaison head body, functional programming > all

---
+++ js/moduleRobot.js
@@ -0,0 +1,8 @@
+let state = {
+numberIndex_head : 0,
+numberIndex_body : 0
+  };
+
+  nexthead();
+  nextbody();
+
--- modulerobot.php
+++ modulerobot.php
@@ -27,7 +27,7 @@ Robots::LoadBODIES();
     <script type="text/javascript" src="layouts/scriptNav.js"></script>
     <link href="layouts/styleNav.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script src="js/creation.js"></script>
+    <script src="js/moduleRobot.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
