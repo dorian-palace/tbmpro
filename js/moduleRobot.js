513402630e6889573295eab5031d6e3b4a805c76
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2022-07-26 22:14:39 +0200

    importation bdd via json ok test d'un prototype de module

--- js/moduleRobot.js
+++ js/moduleRobot.js
@@ -1,8 +1,40 @@
 let state = {
-numberIndex_head : 0,
-numberIndex_body : 0
-  };
+    numberIndex_head : 0,
+    numberIndex_body : 0
+};
 
-  nexthead();
-  nextbody();
+
+let headData = [];
+
+
+fetch('/tbmpro/heads.json')
+.then((res) => res.json())
+.then(function(data) {
+    data.forEach(function(item) {
+        item = item;
+        headData.push(item);
+    });
+    return headData;  
+});
+
+console.log(headData);
+
+nexthead();
+
+
+function nexthead() {
+    let head = document.querySelector("#head");
+    if (state.head < 7) {
+      state.head++;
+      head.setAttribute("class", `head${state.head}`);
+    } else if (state.head === 7) {
+      state.head = 0;
+      head.setAttribute("class", `head${state.head}`);
+    }
+}
+
+
+                
+
+
 
